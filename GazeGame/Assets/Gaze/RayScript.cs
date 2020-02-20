68990662dce29496b1b3c541a5276b053a56c8a9
tolisso <malko01@yandex.ru>
2020-02-20 22:54:17 +0300

    First version of the game part

---
+++ GazeGame/Assets/Gaze/RayScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayScript : MonoBehaviour
+{
+	public Camera gazeCamera;
+	public Ray gazeRay;
+
+	private int x = 200, y = 200;
+    // Start is called before the first frame update
+    void Start()
+    {
+		   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	gazeRay = Camera.main.ScreenPointToRay(new Vector3(x, y, 0));
+    	
+ 		Vector3 direction = gazeRay.direction;
+    	gazeCamera.transform.position = Camera.main.transform.position;
+    	gazeCamera.transform.rotation = Quaternion.Euler(direction[0], direction[1], 0);
+    }
+}
