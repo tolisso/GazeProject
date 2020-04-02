5a71eb9eeba9ca9bb2391fc12880cecd697fea91
unknown <e.malko@bk.ru>
2020-04-02 11:29:13 +0300

    new version of Unity & CubeScript added

---
+++ GazeGame/Assets/Gaze/CubeScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum CameraMode{
+    BigCamera,
+    MediumCamera,
+    SmallCamera,
+    NoneCamera
+}
+
+public class CubeScript : MonoBehaviour
+{
+    public GameObject cubeMinFather;
+    public GameObject cubeSmallFather;
+    public GameObject cubeMediumFather;
+    public GameObject cubeBigFather;
+    
+    public GameObject spawnObject;
+    
+    private GameObject cubeMin;
+    private GameObject cubeSmall;
+    private GameObject cubeMedium;
+    private GameObject cubeBig;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 spawnPoint = spawnObject.transform.position;
+        cubeMin = Instantiate(cubeMinFather, spawnPoint, Quaternion.identity);
+        cubeSmall = Instantiate(cubeSmallFather, spawnPoint, Quaternion.identity);
+        cubeMedium = Instantiate(cubeMediumFather, spawnPoint, Quaternion.identity);
+        cubeBig = Instantiate(cubeBigFather, spawnPoint, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
